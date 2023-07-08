5f976630a42b18f94dfa283a161e79fa6996e360
PalomoLH <luizh.gpalomo@gmail.com>
2023-07-07 23:39:12 -0300

    chore(eslint): adding lint validation

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  settings: {
+    react: { version: 'detect' },
+  },
+  ignorePatterns: ['vite.config.ts', 'dist/'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# react-boilerplate
+# React Boilerplate
+
+This repo is main objective is to have a template to create new repos for react projects.
+It uses Typescript, React, TailwindCss, Jest, Vite, ESLint, Commitlint, Husky packages.
+
+# How to Use Boilerplate
+
+## Clone boilerplate repo
+
+Open remote boilerplate repo at github and add `/generate` at the end of the url
+[Click Here](https://github.com/PalomoLH/react-boilerplate/generate)
+
+## Change Files
+
+- Change `package.json` name and description
+- Change `README.md` title and description
+- Change `LICENSE` year and author
+- Change `src/*` files to match your new project
